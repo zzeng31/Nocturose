8839c421eaae470224741c446d3b0986f9ab841e
zzeng31 <zinuozeng@gmail.com>
2024-02-04 11:32:37 -0800

    eslint setup

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { Route, Routes } from 'react-router-dom';
 import HomePage from './pages/home-page';
 import ProductPage from './pages/product-page';
 function App() {
+  console.log('test');
+
   return (
     <div>
       <Routes>
